40d7cdfbc85496aa50b62ceff274dc7a054c7c4e
Dominic Aglialoro <domiaglialoro@gmail.com>
2023-03-02 18:22:28 -0800

    Version

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,5 +5,5 @@ internal static class PluginInfo
 {
     public const string PLUGIN_GUID = "com.pink.spinrhythm.spincore";
     public const string PLUGIN_NAME = "SpinCore";
-    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_VERSION = "1.0.1";
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using SpinCore;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion($"{PluginInfo.PLUGIN_VERSION}.0")]
+[assembly: AssemblyFileVersion(PluginInfo.PLUGIN_VERSION)]
